931d2ac0ceddd2274cdf79f98872bcffb0147396
Thina <Thina@zipcodersmbp42.mynetworksettings.com>
2022-07-06 18:12:58 -0400

    dice completed, missing bin

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,22 @@
 public class Dice {
+    private int numOfDie;
+
+    public Dice(int numOfDie){
+        this.numOfDie = numOfDie;
+    }
+
+    public int tossAndSum(){
+        int sum = 0;
+        for(int i = 1; i<=numOfDie; i++){
+            sum += Math.floor(Math.random()*6+1);
+        } return sum;
+    }
+
+
+
+
+
+
 
 
 }
---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        Dice die = new Dice(2);
+        die.tossAndSum();
+        System.out.println(die.tossAndSum());
+
+    }
+}
